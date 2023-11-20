6a5a066c75726642d85cc455388282ce1a18f4e9
Gideon <kipkerring.gedion@gmail.com>
2023-11-20 18:11:08 +0300

    auth.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""defines the overall structure of our app"""
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
@@ -7,4 +9,4 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"], strict_slashes=False)
 def home():
     """home page"""
-    return "<h1>Test</h1>"
+    return render_template("home.html")
