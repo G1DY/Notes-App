9ed0b4754dfd90f20892620c94f77fa0ed96f612
Gideon <kipkerring.gedion@gmail.com>
2023-11-20 14:07:10 +0300

    handling the POST method for email sign up

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from flask import Blueprint, render_template
+"""module to define our authetications login, logout and sign up"""
+from flask import Blueprint, render_template, flash, request
 
 auth = Blueprint("auth", __name__)
 
@@ -7,7 +8,8 @@ auth = Blueprint("auth", __name__)
 @auth.route("/login", methods=["GET", "POST"], strict_slashes=False)
 def login():
     """Login page"""
-    return "<h1>Login</h1>"
+    data = request.form.get
+    return render_template("login.html")
 
 
 @auth.route("/logout", methods=["GET", "POST"], strict_slashes=False)
@@ -19,4 +21,24 @@ def logout():
 @auth.route("/sign-up", methods=["GET", "POST"], strict_slashes=False)
 def sign_up():
     """signup to the website"""
-    return "<p>Sign Up</p>"
+    if request.method == "POST":
+        email = request.form.get("email")
+        FullName = request.form.get("fullname") or ""
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if not email or len(email) < 5 or "@" not in email:
+            flash("Email must have atleast 5 characters", category="error")
+        elif len(FullName) < 7 or not FullName:
+            flash("FullNames must have atleast 7 characters", category="error")
+        elif password1 != password2:
+            flash("Password1 must be equal to password2", category="error")
+        elif len(password1) < 7 or len(password2) < 7:
+            flash(
+                "The Password must be atleast 7 characters long", category="error     "
+            )
+        else:
+            flash("Account created!", category="success")
+            pass
+
+    return render_template("sign_up.html")
