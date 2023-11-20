2a4af7caf64e8c25e955d3b39eb71940e8c28f62
Gideon <kipkerring.gedion@gmail.com>
2023-11-20 17:55:43 +0300

    auth.py

--- app/auth.py
+++ app/auth.py
@@ -10,7 +10,18 @@ auth = Blueprint("auth", __name__)
 @auth.route("/login", methods=["GET", "POST"], strict_slashes=False)
 def login():
     """Login page"""
-    data = request.form.get
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in Successfully", category="success")
+            else:
+                flash("Incorrect Password, try again", category="error")
+        else:
+            flash("Email does not exist", category="error")
     return render_template("login.html")
 
 
@@ -29,7 +40,10 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        if not email or len(email) < 5 or "@" not in email:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("User already exists", category="error")
+        elif not email or len(email) < 5 or "@" not in email:
             flash("Email must have atleast 5 characters", category="error")
         elif len(FullName) < 7 or not FullName:
             flash("FullNames must have atleast 7 characters", category="error")
