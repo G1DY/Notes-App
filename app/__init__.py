42dc32a2411b19b911cbb1b3f49639630b664508
Gideon <kipkerring.gedion@gmail.com>
2023-12-07 19:14:36 +0300

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """sets up and runs our notes app"""
 from flask import Flask
-from .views import views
-from .auth import auth
 from flask_sqlalchemy import SQLAlchemy
-from .models import User, Note
 from os import path
 from flask_login import LoginManager
 
@@ -13,16 +10,21 @@ DB_NAME = "database.db"
 
 
 def create_app():
-    """function that starts our app"""
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "mypassword"
+    app.config["SECRET_KEY"] = "hjshjhdjah kjshkjdhjs"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
+
+    from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    create_database(app)
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -30,14 +32,12 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        """validate user from db"""
         return User.query.get(int(id))
 
     return app
 
 
-def create_database():
-    """creates database"""
-    if not path.exists("app/" + DB_NAME):
-        with app.app_context():
-            db.create_all()
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
